43a9ee281747de0592fe8b9583ca0e1503e59e4c
Nguyen Cong Phong <barcatrongtim2@gmail.com>
2023-04-07 02:42:40 +0700

    up code home

---
+++ BTL_Android_TKB/app/src/main/java/com/example/btl_android_tkb/TKB/sukien/Sup_SuKien.java
@@ -0,0 +1,6 @@
+package com.example.btl_android_tkb.TKB.sukien;
+
+public class Sup_SuKien {
+    public static UTLLich utlLich = new UTLLich();
+}
+
---
+++ BTL_Android_TKB/app/src/main/java/com/example/btl_android_tkb/sup/truyen/TruyenData.java
@@ -0,0 +1,9 @@
+package com.example.btl_android_tkb.sup.truyen;
+
+import com.codernon2k1.note.sup.utl.Monan;
+import com.codernon2k1.note.zhome.tienich.sach.UTLSach;
+
+public class TruyenData {
+    public static Monan monan = new Monan();
+    public static UTLSach truyen = new UTLSach();
+}
